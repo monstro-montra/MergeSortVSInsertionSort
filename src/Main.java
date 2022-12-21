dfc3e6752507d3ebe9530e6b352653f8822d8e38
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-12-21 02:18:57 -0500

    added a method to get user input for the capacity of the arrays and finished merge sort methods. added longs to get mergeSortStartTime, mergeSortEndTime, and mergeSortDuration. This will get the duration it takes to execute the mergeSort method in nanoseconds and milliseconds.

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,70 @@
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array1 = new int[100]; //declare an array with a capacity of 100 elements
+        int capacity = getUserInputForCapacity(); //call getUserInputForCapacity method to get the capacity of array1.
+
+        int[] array1 = new int[capacity]; //declare an array with a capacity of (capacity) elements
         fillArray(array1); //call fillArray function on array1
         printArray(array1); //call printArray function on array1
+
+        System.out.println("\n");//two empty nl
+        long mergeSortStartTime = System.nanoTime(); //start time for mergeSort method call
+        mergeSort(array1); //call mergeSort function on array1
+        long mergeSortEndTime = System.nanoTime(); // end time for mergeSort method call
+        long mergeSortDuration = mergeSortEndTime - mergeSortStartTime;
+        System.out.println("Merge sort elapsed time: " + mergeSortDuration + " nanoseconds, which is "
+                + TimeUnit.NANOSECONDS.toMillis(mergeSortDuration) + " milliseconds."); // print elapsed time in nanoseconds and milliseconds
+
+        printArray(array1); // call printArray function on array1
     }
 
     static void fillArray(int[] array){ // method used for filling array with random ints
         Random rand = new Random(); //create new random object
         for (int i = 0; i < array.length; i++){ //iterate as long as i < array capacity
-            array[i] = rand.nextInt(100); //random int between 0 and 99
+            array[i] = rand.nextInt(array.length + 1); //random int between 0 and length of array + 1 (inclusive)
         }
     }
 
     static void printArray(int[] array){ // method used for printing the array contents
         for (int j : array) { //enhanced for loop to iterate through entire array
-            System.out.println(j); //print j
+            System.out.print(j + " "); //print j
         }
     }
 
-    static void merge(int arr[], int[] left, int right[]){
+    //NEXT TWO METHODS ARE FOR MERGE SORT
+    static void merge(int[] arr, int[] left, int[] right){
         int i = 0, indexLeft = 0, indexRight = 0;
+        int leftSize = arr.length / 2;
+        int rightSize = arr.length - leftSize;
 
-        //while the index of left array < length of left array and index of right array is < length of right array.
+        //while the index of left array < leftSize and index of right array is < rightSize.
         // while there are elements in left and right
-        while(indexLeft < left.length && indexRight < right.length){
-            if(left[indexLeft] < right[indexRight]){
-
+         while(indexLeft < leftSize && indexRight < rightSize){
+            if(left[indexLeft] < right[indexRight]){ //if left array element at indexLeft is < right array element at indexRight
+                arr[i] = left[indexLeft]; //set arr element at i = to left array element at indexRight
+                i++; //increment i
+                indexLeft++; //increment indexLeft
+            } else { //if above condition is not true
+                arr[i] = right[indexRight]; //set arr element at i = to right array element at indexRight
+                i++; //increment i
+                indexRight++; //increment indexRight
             }
         }
-
-
+        //in case there is an element remaining that cannot be compared to another element since remainder is one
+        while (indexLeft < left.length){
+            arr[i] = left[indexLeft]; //set arr[i] = to element in the left array at indexLeft
+            i++; //increment i
+            indexLeft++; //increment indexLeft
+        }
+        while (indexRight < right.length){
+            arr[i] = right[indexRight]; //set arr[i] = to element in right array at indexRight
+            i++; //increment i
+            indexRight++; //increment indexRight
+        }
     }
 
     static void mergeSort(int[] arr){
@@ -41,21 +74,48 @@ public class Main {
         }
 
         int midpoint = arr.length/2; //mid-point is half the capacity of array
-        int[] left = new int[midpoint]; //left subarray will go from arr[0] to midpoint (arr.length / 2)
-        int[] right = new int[arr.length - midpoint]; //right subarray will go from mid-point to array.length
+        int[] left = new int[midpoint]; //left sub-array will go from arr[0] to midpoint (arr.length / 2)
+        int[] right = new int[arr.length - midpoint]; //right sub-array will go from mid-point to array.length
 
-        int j = 0; //use for right array
-        for(int i = 0; i < arr.length; i++){ //iterate as long as i is less than arr.length
+        int i = 0;
+        int j = 0;
+        //use for right array
+        for(; i < arr.length; i++){ //iterate as long as i is less than arr.length
             if(i < midpoint){ // if i is less than the midpoint
-                left[i] = arr[i]; //set left[i] = arr[i]. add this value to the left subarray
+                left[i] = arr[i]; //set left[i] = arr[i]. add this value to the left sub-array
             } else {
-                right[i] = arr[i]; //set right[i] = arr[i]. add this value to right subarray
-                j++; //add 1 to j
+                right[j] = arr[i]; //set right[j] = arr[i]. add this value to right sub-array
+                j++; //increment j
             }
         }
         mergeSort(left); //recursively call mergeSort on left subArray to once again make another division
         mergeSort(right); //recursively call mergeSort on right subArray to once again make another division
         merge(arr, left, right); //call merge on arr, left, and right arrays
+    }
+
+    static int getUserInputForCapacity(){ //this method simply gets the user input used for the capacity of the array.
+        int userInput;
+        Scanner in = new Scanner(System.in);
+        while(true) { // start while loop
+            System.out.print("Input an integer that will be used for the capacity of an array:"); //tell user to input an integer
+            try { //start try block
+                if (in.hasNextInt()) { //if next user input is an integer
+                    userInput = in.nextInt(); //userInput = int that user inputs
+                    if(userInput < 0){ //if userInput is less than 0,
+                        throw new IndexOutOfBoundsException("Out of bounds."); //throw IndexOutOfBoundsException
+                    }
+                    return userInput; //otherwise, return user input
+                } else { //otherwise, throw InputMismatchException, meaning the user input something aside from an int
+                    throw new InputMismatchException("Incorrect input.");
+                }
+            } catch (InputMismatchException er){ //catch InputMismatchException
+                System.out.println(er.getMessage());
+                in.next(); //prevent infinite loop
+            } catch (IndexOutOfBoundsException er) { //catch IndexOutOfBoundsException
+                System.out.println(er.getMessage());
+            }
+        }
+
     }
 
 }
