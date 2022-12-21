50bb6250ac21530edb7b2c66b02aa8aa718700a3
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-12-21 02:34:31 -0500

    added a method to get user input for the capacity of the arrays and finished merge sort methods. added longs to get mergeSortStartTime, mergeSortEndTime, and mergeSortDuration. This will get the duration it takes to execute the mergeSort method in nanoseconds and milliseconds.

--- src/Main.java
+++ src/Main.java
@@ -22,77 +22,6 @@ public class Main {
         printArray(array1); // call printArray function on array1
     }
 
-    static void fillArray(int[] array){ // method used for filling array with random ints
-        Random rand = new Random(); //create new random object
-        for (int i = 0; i < array.length; i++){ //iterate as long as i < array capacity
-            array[i] = rand.nextInt(array.length + 1); //random int between 0 and length of array + 1 (inclusive)
-        }
-    }
-
-    static void printArray(int[] array){ // method used for printing the array contents
-        for (int j : array) { //enhanced for loop to iterate through entire array
-            System.out.print(j + " "); //print j
-        }
-    }
-
-    //NEXT TWO METHODS ARE FOR MERGE SORT
-    static void merge(int[] arr, int[] left, int[] right){
-        int i = 0, indexLeft = 0, indexRight = 0;
-        int leftSize = arr.length / 2;
-        int rightSize = arr.length - leftSize;
-
-        //while the index of left array < leftSize and index of right array is < rightSize.
-        // while there are elements in left and right
-         while(indexLeft < leftSize && indexRight < rightSize){
-            if(left[indexLeft] < right[indexRight]){ //if left array element at indexLeft is < right array element at indexRight
-                arr[i] = left[indexLeft]; //set arr element at i = to left array element at indexRight
-                i++; //increment i
-                indexLeft++; //increment indexLeft
-            } else { //if above condition is not true
-                arr[i] = right[indexRight]; //set arr element at i = to right array element at indexRight
-                i++; //increment i
-                indexRight++; //increment indexRight
-            }
-        }
-        //in case there is an element remaining that cannot be compared to another element since remainder is one
-        while (indexLeft < left.length){
-            arr[i] = left[indexLeft]; //set arr[i] = to element in the left array at indexLeft
-            i++; //increment i
-            indexLeft++; //increment indexLeft
-        }
-        while (indexRight < right.length){
-            arr[i] = right[indexRight]; //set arr[i] = to element in right array at indexRight
-            i++; //increment i
-            indexRight++; //increment indexRight
-        }
-    }
-
-    static void mergeSort(int[] arr){
-        //this method handles the division of arrays by two. it will recursively make divisions until arr.length <= 1.
-        if (arr.length <= 1){ //if array length is <= 1
-            return; //base case
-        }
-
-        int midpoint = arr.length/2; //mid-point is half the capacity of array
-        int[] left = new int[midpoint]; //left sub-array will go from arr[0] to midpoint (arr.length / 2)
-        int[] right = new int[arr.length - midpoint]; //right sub-array will go from mid-point to array.length
-
-        int i = 0;
-        int j = 0;
-        //use for right array
-        for(; i < arr.length; i++){ //iterate as long as i is less than arr.length
-            if(i < midpoint){ // if i is less than the midpoint
-                left[i] = arr[i]; //set left[i] = arr[i]. add this value to the left sub-array
-            } else {
-                right[j] = arr[i]; //set right[j] = arr[i]. add this value to right sub-array
-                j++; //increment j
-            }
-        }
-        mergeSort(left); //recursively call mergeSort on left subArray to once again make another division
-        mergeSort(right); //recursively call mergeSort on right subArray to once again make another division
-        merge(arr, left, right); //call merge on arr, left, and right arrays
-    }
-
     static int getUserInputForCapacity(){ //this method simply gets the user input used for the capacity of the array.
         int userInput;
         Scanner in = new Scanner(System.in);
@@ -116,6 +45,84 @@ public class Main {
             }
         }
 
+    }
+
+    static void fillArray(int[] array){ // utility method used for filling array with random ints
+        Random rand = new Random(); //create new random object
+        for (int i = 0; i < array.length; i++){ //iterate as long as i < array capacity
+            array[i] = rand.nextInt(array.length + 1); //random int between 0 and length of array + 1 (inclusive)
+        }
+    }
+
+    static void printArray(int[] array){ // utility method used for printing the array contents
+        for (int j : array) { //enhanced for loop to iterate through entire array
+            System.out.print(j + " "); //print j
+        }
+    }
+
+    //NEXT TWO METHODS ARE FOR MERGE SORT
+    static void merge(int[] arr, int[] leftArr, int[] rightArr){
+        int i = 0, indexLeft = 0, indexRight = 0;
+        int leftSize = arr.length / 2;
+        int rightSize = arr.length - leftSize;
+
+        //while the index of left array < leftSize and index of right array is < rightSize.
+        //while there are elements in left and right
+         while(indexLeft < leftSize && indexRight < rightSize){
+            if(leftArr[indexLeft] < rightArr[indexRight]){ //if left array element at indexLeft is < right array element at indexRight
+                arr[i] = leftArr[indexLeft]; //set arr element at i = to left array element at indexRight
+                i++; //increment i
+                indexLeft++; //increment indexLeft
+            } else { //if above condition is not true
+                arr[i] = rightArr[indexRight]; //set arr element at i = to right array element at indexRight
+                i++; //increment i
+                indexRight++; //increment indexRight
+            }
+        }
+        //in case there is an element remaining that cannot be compared to another element since remainder is one
+        while (indexLeft < leftArr.length){
+            arr[i] = leftArr[indexLeft]; //set arr[i] = to element in the left array at indexLeft
+            i++; //increment i
+            indexLeft++; //increment indexLeft
+        }
+        while (indexRight < rightArr.length){
+            arr[i] = rightArr[indexRight]; //set arr[i] = to element in right array at indexRight
+            i++; //increment i
+            indexRight++; //increment indexRight
+        }
+    }
+
+    static void mergeSort(int[] arr){
+        //this method handles the division of arrays by two. it will recursively make divisions until arr.length <= 1.
+        if (arr.length <= 1){ //if array length is <= 1
+            return; //base case
+        }
+
+        int midpoint = arr.length/2; //mid-point is half the capacity of array
+        int[] left = new int[midpoint]; //left sub-array will go from arr[0] to midpoint (arr.length / 2)
+        int[] right = new int[arr.length - midpoint]; //right sub-array will go from mid-point to array.length
+
+        int j = 0;
+        //use for right array
+        for(int i = 0; i < arr.length; i++){ //iterate as long as i is less than arr.length
+            if(i < midpoint){ // if i is less than the midpoint
+                left[i] = arr[i]; //set left[i] = arr[i]. add this value to the left sub-array
+            } else {
+                right[j] = arr[i]; //set right[j] = arr[i]. add this value to right sub-array
+                j++; //increment j
+            }
+        }
+        mergeSort(left); //recursively call mergeSort on left subArray to once again make another division
+        mergeSort(right); //recursively call mergeSort on right subArray to once again make another division
+        merge(arr, left, right); //call merge on arr, left, and right arrays
+    }
+
+    //NEXT METHOD IS INSERTION SORT METHOD
+    static void insertionSort(int[] arr){ //TODO
+        int length = arr.length;
+        for (int i = 1; i < length; i++){
+
+        }
     }
 
 }
